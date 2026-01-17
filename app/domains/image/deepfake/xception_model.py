407291987c2c28ae784141b7f214b5a9175dc9c1
HeeJeongSeo <heejung9965@naver.com>
2026-01-17 10:57:59 +0900

    feat: XceptionNet 커스텀 FF++ 학습 구조 적용 및 추론 파이프라인 정리

--- app/domains/image/deepfake/xception_model.py
+++ app/domains/image/deepfake/xception_model.py
@@ -4,14 +4,16 @@ import torch.nn as nn
 import timm
 
 class XceptionNet(nn.Module):
-    def __init__(self, pretrained: bool = True):
+    def __init__(self, pretrained=True):
         super().__init__()
-
         self.backbone = timm.create_model(
             "xception",
             pretrained=pretrained,
-            num_classes=1 
+            num_classes=0,  
+            global_pool="avg"
         )
+        self.classifier = nn.Linear(2048, 1)
 
     def forward(self, x):
-        return self.backbone(x)
+        feat = self.backbone(x)
+        return self.classifier(feat)
